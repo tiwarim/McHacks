91c4d80bca956ea54dc61937782b6eddd2c21b3b
Dima Shil <dima.shilchikov@gmail.com>
2020-02-01 14:42:38 -0500

    view added

--- Mpath/Mpathy/Mpathy/settings.py
+++ Mpath/Mpathy/Mpathy/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend') 
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static'),
+]
--- Mpath/Mpathy/Mpathy/urls.py
+++ Mpath/Mpathy/Mpathy/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import url
+from userSearch.views import userSearch, FrontendAppView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('userSearch', userSearch)
+    path('url_checker', userSearch),
+    url(r'^', FrontendAppView.as_view())
 ]
