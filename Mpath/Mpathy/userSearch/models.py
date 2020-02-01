1a676f5078ee05a2f5c6c25da27528366626cccb
Dima Shil <dima.shilchikov@gmail.com>
2020-02-01 15:46:22 -0500

    .

--- Mpath/Mpathy/userSearch/models.py
+++ Mpath/Mpathy/userSearch/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+      userName = models.CharField(max_length=120)
+      description = models.TextField()
+      boolTest = models.BooleanField(default=False)
+
+      def _str_(self):
+        return self.title
