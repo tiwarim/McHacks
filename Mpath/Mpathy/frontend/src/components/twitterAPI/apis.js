3ca61808e1480c8aa68c9011077fed960d78a689
tiwarim <tiwarim@mcmaster.ca>
2020-02-01 23:40:04 -0500

    Update apis.js
    added the two api calls to get usename and tweets

--- Mpath/Mpathy/frontend/src/components/twitterAPI/apis.js
+++ Mpath/Mpathy/frontend/src/components/twitterAPI/apis.js
@@ -1,19 +1,25 @@
 import axios from "axios";
 
 export const apiMethods = {
-  getTestDataNoParams,
-  getTestDataWithParams
+  getUsernames,
+  getTweets
 };
 
-async function getTestDataNoParams() {
-  return await axios("https://swapi.co/api/planets/8/").then(response => {
-    return response.data;
-  });
-}
+// this fnc returns whole bunch of users
+async function getUsernames() {
+  let config = { params: {q:data} }
+  return await axios("https://api.twitter.com/1.1/users/search.json", config).then(
+    response=>{return response.data}
+    )
+  
+  };
 
-// data has 2 params name, id
-async function getTestDataWithParams(data) {
-  let config = { params: { name: data.name, id: data.id } };
+// at this point, the parent knows the username and id of the child
 
-  return await axios("https://swapi.co/api/planets/8/", config);
+// we use the id to get some past tweets of the child
+async function getTweets(data) {
+  let config = { params: { id: data} };
+  return await axios("https://api.twitter.com/1.1/statuses/user_timeline.json", config).then(
+    response=>{return response.data}
+    )
 }
